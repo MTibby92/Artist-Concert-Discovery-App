d659ef626f38a40d943b75ace67e5c9c76e97e5d
MTibby92 <MTibby92@gmail.com>
2016-09-17 11:39:09 -0500

    created firebase v2 which uses a pop-up sigin in so the page doesn't refresh

--- assets/javascript/firebase-v2.js
+++ assets/javascript/firebase-v2.js
@@ -9,7 +9,6 @@ var config = {
 firebase.initializeApp(config)
 
 
-
 /**
  * Function called when clicking the Login/Logout button.
  */
@@ -23,7 +22,32 @@ function toggleSignIn() {
         provider.addScope('https://www.googleapis.com/auth/plus.login');
         // [END addscopes]
         // [START signin]
-        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // [START_EXCLUDE]
+            document.getElementById('oauthtoken').textContent = token;
+            // [END_EXCLUDE]
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/account-exists-with-different-credential') {
+                alert('You have already signed up with a different auth provider for that email.');
+                // If you are using multiple auth providers on your app you should handle linking
+                // the user's accounts here.
+            } else {
+                console.error(error);
+            }
+            // [END_EXCLUDE]
+        });
         // [END signin]
     } else {
         // [START signout]
@@ -39,43 +63,8 @@ function toggleSignIn() {
  * initApp handles setting up UI event listeners and registering Firebase auth listeners:
  *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
  *    out, and that is where we update the UI.
- *  - firebase.auth().getRedirectResult(): This promise completes when the user gets back from
- *    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
  */
 function initApp() {
-    // Result from Redirect auth flow.
-    // [START getidptoken]
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // [START_EXCLUDE]
-            document.getElementById('oauthtoken').textContent = token;
-        } else {
-            document.getElementById('oauthtoken').textContent = 'null';
-            // [END_EXCLUDE]
-        }
-        // The signed-in user info.
-        var user = result.user;
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // [START_EXCLUDE]
-        if (errorCode === 'auth/account-exists-with-different-credential') {
-            alert('You have already signed up with a different auth provider for that email.');
-            // If you are using multiple auth providers on your app you should handle linking
-            // the user's accounts here.
-        } else {
-            console.error(error);
-        }
-        // [END_EXCLUDE]
-    });
-    // [END getidptoken]
     // Listening for auth state changes.
     // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function(user) {
@@ -89,7 +78,8 @@ function initApp() {
             var uid = user.uid;
             var providerData = user.providerData;
             // [START_EXCLUDE]
-            document.getElementById('sign-in-status').textContent = 'Signed in';
+            // document.getElementById('sign-in-status').textContent = 'Signed in';
+            console.log('Signed In')
             document.getElementById('sign-in').textContent = 'Sign out';
             $('#sign-in').html('Sign-Out')
             // document.getElementById('account-details').textContent = JSON.stringify(user, null, '  ');
@@ -97,7 +87,8 @@ function initApp() {
         } else {
             // User is signed out.
             // [START_EXCLUDE]
-            document.getElementById('sign-in-status').textContent = 'Signed out';
+            // document.getElementById('sign-in-status').textContent = 'Signed out';
+            console.log('Signed Out')
             document.getElementById('sign-in').textContent = 'Sign in with Google';
             $('#sign-in').html('Sign-In')
             // document.getElementById('account-details').textContent = 'null';
@@ -111,10 +102,10 @@ function initApp() {
     // [END authstatelistener]
     document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
 }
-window.onload = function() {
-    initApp();
-}
+// window.onload = function() {
+//     initApp();
+// }
 
 $( document ).ready(function() {
-	$('#sign-in').on('click', toggleSignIn)
+    $('#sign-in').on('click', initApp)
 })
